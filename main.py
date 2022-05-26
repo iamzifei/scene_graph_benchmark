9d8c0cb51b7a3d08e3031b781e6e1322a2897806
iamzifei <iamzifei@gmail.com>
2022-05-26 12:42:46 +1000

    adding zip
    update docker build, zip image file after processing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+start_dir = 0  # start from this image directory, inclusive
 meta_file = './waterbirds/metadata.csv'
 df = pd.read_csv(
     meta_file,
@@ -13,11 +14,16 @@ dirs = np.unique([img_filename.split('/')[0]
 
 # loop dirs, list all files inside each dir
 for dir in tqdm(dirs):
+    dir_num = int(dir.split('.')[0])
     dir = './waterbirds/images/' + dir
     print(dir)
+    if dir_num < start_dir:
+        continue
     # loop each file in dir
     for file in tqdm(os.listdir(dir)):
         # if file ends with .jpg, and not ends with .detect.jpg
         if file.endswith('.jpg') and not file.endswith('.detect.jpg'):
             os.system(
                 'python tools/demo/demo_image.py --config_file sgg_configs/vgattr/vinvl_x152c4.yaml --img_file ' + dir + '/' + file + ' MODEL.WEIGHT pretrained_model/vinvl_vg_x152c4.pth MODEL.ROI_HEADS.NMS_FILTER 1 MODEL.ROI_HEADS.SCORE_THRESH 0.2 TEST.IGNORE_BOX_REGRESSION False')
+    os.system('zip -r ' + dir + '.zip ' + dir)
+    print('saved to ' + dir + '.zip')
